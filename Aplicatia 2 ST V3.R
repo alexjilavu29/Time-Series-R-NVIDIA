ca8770eca87b05d85d112ace7a0f985f444139ff
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-05-29 22:47:01 +0300

    Add Third Version to Application 2 Project -- 6th Commit

--- Aplicatia 2 ST V3.R
+++ Aplicatia 2 ST V3.R
@@ -28,6 +28,12 @@ library(TSA)
 library(FinTS)
 setwd("~/Facultate/ASE/Anul 3/Semestrul 2/Serii de Timp/Proiect ST")
 
+# Citirea cresterii pib-ului din fisierul crestere_pib.csv si crearea unui dataframe
+crestere_pib <- read.csv("crestere_pib.csv", header = TRUE, sep = ",")
+crestere_pib <- crestere_pib[2:32,]
+crestere_pib <- crestere_pib[order(crestere_pib$Ani),]
+
+
 # Citirea valorilor din fisierul absolventi.csv si crearea unui dataframe
 absolventi <- read.csv("absolventi.csv", header = TRUE, sep = ",")
 absolventi <- absolventi[2:32,]
@@ -37,11 +43,6 @@ absolventi <- absolventi[order(absolventi$Ani),]
 unitati_invatamant <- read.csv("unitati_invatamant.csv", header = TRUE, sep = ",")
 unitati_invatamant <- unitati_invatamant[2:32,]
 unitati_invatamant <- unitati_invatamant[order(unitati_invatamant$Ani),]
-
-# Citirea cresterii pib-ului din fisierul crestere_pib.csv si crearea unui dataframe
-crestere_pib <- read.csv("crestere_pib.csv", header = TRUE, sep = ",")
-crestere_pib <- crestere_pib[2:32,]
-crestere_pib <- crestere_pib[order(crestere_pib$Ani),]
 
 # Citirea numarului de biblioteci din fisierul biblioteci.csv si crearea unui dataframe
 biblioteci <- read.csv("biblioteci.csv", header = TRUE, sep = ",")
@@ -73,7 +74,7 @@ ggplot(data = absolventi_unitati) +
   ggtitle('Norul de puncte dintre nr de unitati de invatamant si % de crestere a PIB-ului')+
   theme_bw()
 
-# Declaram variabilele de tip ts
+# Declararea variabilelor de tip „time series”
 absolventi_ts <- ts(absolventi_unitati$Absolventi, start = 1991, frequency = 1)
 unitati_ts <- ts(absolventi_unitati$Unitati, start = 1991, frequency = 1)
 crestere_pib_ts <- ts(absolventi_unitati$`Crestere PIB`, start = 1991, frequency = 1)
@@ -92,7 +93,7 @@ crestere_pib_ts <- (crestere_pib_ts - mean(crestere_pib_ts)) / sd(crestere_pib_t
 biblioteci_ts <- (biblioteci_ts - mean(biblioteci_ts)) / sd(biblioteci_ts)
 
 
-# Calculul procentajelor de schimbare
+# Calculul primei diferențe
 absolventi_diff <- diff(absolventi_ts)
 unitati_diff <- diff(unitati_ts)
 crestere_pib_diff <- diff(crestere_pib_ts)
@@ -117,8 +118,9 @@ autoplot(cbind(absolventi_diff,unitati_diff,crestere_pib_diff,biblioteci_diff)) 
 # Determinarea persistentei modelului
 ggtsdisplay(absolventi_ts)
 ggtsdisplay(unitati_ts)
-ggtsdisplay(crestere_pib_ts)
 ggtsdisplay(biblioteci_ts)
+
+ggtsdisplay(crestere_pib_ts)
 
 ggtsdisplay(absolventi_diff)
 ggtsdisplay(unitati_diff)
@@ -126,19 +128,18 @@ ggtsdisplay(crestere_pib_diff)
 ggtsdisplay(biblioteci_diff)
 
 
-# Testarea stationaritatii seriilor (am ales varianta cea mai complexa a ADF)
+# Testarea stationaritatii seriilor 
 adf.absolventi <- ur.df(absolventi_ts, type = "trend", selectlags = "AIC")
-summary(adf.absolventi) # serie 
+summary(adf.absolventi) # serie nestationara
 
 adf.unitati <- ur.df(unitati_ts, type = "trend", selectlags = "AIC")
-summary(adf.unitati) # serie 
-
-adf.crestere_pib <- ur.df(crestere_pib_ts, type = "trend", selectlags = "AIC")
-summary(adf.crestere_pib) # serie 
+summary(adf.unitati) # serie nestationara
 
 adf.biblioteci <- ur.df(biblioteci_ts, type = "trend", selectlags = "AIC")
-summary(adf.biblioteci) # serie
+summary(adf.biblioteci) # serie nestationara
 
+adf.crestere_pib <- ur.df(crestere_pib_ts, type = "trend", selectlags = "AIC")
+summary(adf.crestere_pib) # serie stationara
 
 ndiffs(absolventi_ts)
 ndiffs(unitati_ts)
